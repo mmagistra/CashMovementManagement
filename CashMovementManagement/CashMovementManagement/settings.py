1f690a356b94f0b09fd1b82676130f4ad0315148
domini <mikalmet@gmail.com>
2025-04-14 08:02:57 +0500

    update .env

--- CashMovementManagement/CashMovementManagement/settings.py
+++ CashMovementManagement/CashMovementManagement/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sgebv5y=1kqr4(fje0!o!uiax%nicbw*fm5h9n!9a$u%ujpaq^'
+SECRET_KEY = os.getenv('SECRET_KEY', default='django-insecure-sgebv5y=1kqr4(fje0!o!uiax%nicbw*fm5h9n!9a$u%ujpaq^')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = not production_mode
